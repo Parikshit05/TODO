1e51315e28be3094c4d0c72cbb52db821f7ba89f
ParikshitPandey <parikshitpandey03@gmail.com>
2025-04-06 21:05:34 +0530

    Fixed few things

--- app.js
+++ app.js
@@ -22,28 +22,3 @@ ul.addEventListener("click",function(event){
     let listItem = event.target.parentElement;
     listItem.remove();
 })
-
-const cursor = document.querySelectorAll(".cursor");
-const links = document.querySelectorAll(".link");
-
-window.addEventListener("mousemove", (e) => {
-  
-  let x = e.pageX;
-  let y = e.pageY;
-  
-  cursor.forEach(el => {
-    el.style.left = `${x}px`;
-    el.style.top = `${y}px`;
-    
-    links.forEach(link => {
-      link.addEventListener("mouseenter", () => {
-        el.classList.add("hover");
-      })
-      link.addEventListener("mouseleave", () => {
-        el.classList.remove("hover");
-      })
-    })
-    
-  })
-  
-})
